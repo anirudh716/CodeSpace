8cbc240ab356417d73b7c97d049d9a3d0e9bc14f
anirudh716 <anirudhpandey244@gmail.com>
2023-10-08 02:14:15 +0530

    Added I/o box
    Added I/o box and added syntax highlighting for different lang

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ var editor = CodeMirror.fromTextArea(document.getElementById('editor'),{
         autoCloseBrackets: true,
     }
 );
+
+var option = document.getElementById("languages");
+option.addEventListener("change", function(){
+    if (option.value == "c"){
+        editor.setOption("mode","text/x-csrc");
+    }
+    else if (option.value == "java"){
+        editor.setOption("mode","text/x-java");
+    }
+    else if (option.value == "cpp"){
+        editor.setOption("mode","text/x-c++src");
+    }
+    else if (option.value == "python"){
+        editor.setOption("mode","text/x-python");
+    }
+});
+
